d7f35d609ed24b01b12690b2649f2974754101cc
cam-py <ctchokouani@gmail.com>
2022-09-14 22:41:21 -0400

    added bootstrap files

---
+++ site/js/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+
+ const navList = document.querySelector('.nav-list--iu');
+const  mobileMenu = document.querySelector('.mobile-menu')
+
+//Starting Conditions
+//navList.classList.add('hidden')
+//mobileMenu.classList.remove('hidden')
+
